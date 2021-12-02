371fcf0fbd60c707af7b73ce7717735c2129ab19
STIRELLA2 <90012377+STIRELLA2@users.noreply.github.com>
2021-12-01 22:34:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def flask_import():
 <h2>Group 8: Zemin Li, Sherri Putnam, Spencer Tirella</h2>
   <title>FA595 Grp 8 Final Project
   
+  The below application is designed to accept a stock ticker, pull the company's 2020 10K filing from the SEC Edgar database, and return the sentiment analysis score for the company's comments in section 1A "Risk Factors" of the filing.
   </title>
 </head>
     <form action="/text" method="post">

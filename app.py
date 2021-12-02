457fd2e35faef05fdd7d07827a4ba4ddab107a23
STIRELLA2 <90012377+STIRELLA2@users.noreply.github.com>
2021-12-01 21:58:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -47,9 +47,11 @@ print(score)
 
 app = Flask(__name__) # "__main__"
 
-@app.route('/grp8', methods=['GET', 'POST'])
+@app.route('/')
 def index():
    return render_template("index.html")
+
+@app.route('/grp8', methods=['GET', 'POST'])
 def flask_import():
   return """<html>
   <h1>FA595 Final Project<h1><br><br>

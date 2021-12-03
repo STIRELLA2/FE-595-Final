ce80a76903408e89927ff1a46c2edb2136d8e5c6
STIRELLA2 <90012377+STIRELLA2@users.noreply.github.com>
2021-12-03 18:15:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,47 +10,11 @@ from sec_api import XbrlApi
 from sec_api import ExtractorApi
 from sec_api import RenderApi
 
-## Grab the URL to the 2020 10K for a given ticker
-from sec_api import QueryApi
-queryApi = QueryApi(api_key="bddda2de3ae47b101a2c2a2a94c09591ab98481b5a2fe1a7fda21ab0c14809f6")
-
-query = {
-  "query": { "query_string": { 
-      "query": "ticker:TRV AND filedAt:{2020-01-01 TO 2020-12-31} AND formType:\"10-k\"" 
-    } },
-  "from": "0",
-  "size": "10",
-  "sort": [{ "filedAt": { "order": "desc" } }]
-}
-  
-
-filings = queryApi.get_filings(query)
-
-## Take the URL to the 10K and extract section 1 Management Commentary
-from sec_api import ExtractorApi
-
-extractorApi = ExtractorApi("bddda2de3ae47b101a2c2a2a94c09591ab98481b5a2fe1a7fda21ab0c14809f6")
-
-# 10-K filing
-filing_url = "https://www.sec.gov/ix?doc=/Archives/edgar/data/86312/000008631220000011/trv-12312019x10k.htm"
-
-# get the standardized and cleaned text of section 1A "Risk Factors"
-section_text = extractorApi.get_section(filing_url, "1A", "text")
-
-import nltk
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-sid=SentimentIntensityAnalyzer()
-sentence=section_text
-score=sid.polarity_scores(sentence)
-
-
 app = Flask(__name__) # "__main__"
 
 @app.route('/grp8', methods=['GET', 'POST'])
 def flask_import():
-  
-  
+   
   return f"""<html>
 
   <h1>FA595 Final Project<h1><br><br>
@@ -66,11 +30,7 @@ def flask_import():
     <input type="submit" value="Submit">
   </form>
   
-
-  <tr>{score}</tr>
-  
-  
-  
+  <tr>{score}</tr>  
   <body>
  <!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
@@ -109,7 +69,54 @@ def flask_import():
   </html>
   """
 
+@app.before_request
+def before():
+    print('FE 595 Group 8: Zemin Li, Sherri Putnam, Spencer Tirella')
 
+@app.route('/ticker', methods=['GET', 'POST'])
+def sec():
+  tickerfromuser={'string':request.json['string']}
+  tickerfromuser2=json.dumps(tickerfromuser)
+  
+  ## Grab the URL to the 2020 10K for a given ticker
+  from sec_api import QueryApi
+  queryApi = QueryApi(api_key="bddda2de3ae47b101a2c2a2a94c09591ab98481b5a2fe1a7fda21ab0c14809f6")
+
+  query = {
+  "query": { "query_string": { 
+      "query": "ticker:"tickerfromuser2 "AND filedAt:{2020-01-01 TO 2020-12-31} AND formType:\"10-k\"" 
+    } },
+  "from": "0",
+  "size": "10",
+  "sort": [{ "filedAt": { "order": "desc" } }]
+  }
+  
+filings = queryApi.get_filings(query)
+
+## Take the URL to the 10K and extract section 1 Management Commentary
+from sec_api import ExtractorApi
+
+extractorApi = ExtractorApi("bddda2de3ae47b101a2c2a2a94c09591ab98481b5a2fe1a7fda21ab0c14809f6")
+
+# 10-K filing
+filing_url = "https://www.sec.gov/ix?doc=/Archives/edgar/data/86312/000008631220000011/trv-12312019x10k.htm"
+
+# get the standardized and cleaned text of section 1A "Risk Factors"
+section_text = extractorApi.get_section(filing_url, "1A", "text")
+
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sid=SentimentIntensityAnalyzer()
+sentence=section_text
+score=sid.polarity_scores(sentence)
+
+    return jsonify({'':score})
+
+#Error Handling
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'This page does not exist', 404
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=6666)
